85e9c94bdd9a3991eb5fde6c01877d19d9935a00
Stanisław Horna <stanislawhorna@outlook.com>
2024-04-27 14:37:24 +0200

    move opencv related code to dedicated class

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ import copy
 
 from dependencies.video.video import Video
 
+VIDEO_FILE_PATH = "./assets/conveyor-jewelry.mp4"
 
 def main():
-    video = Video(path="./assets/conveyor-jewelry.mp4")
-    video.get_frame()
+    video = Video(path=VIDEO_FILE_PATH)
+    
+    while not video.is_ended():
+        
+        fr = video.get_frame()
+        video.show_frame(fr)
+    
 
 
 if __name__ == "__main__":
