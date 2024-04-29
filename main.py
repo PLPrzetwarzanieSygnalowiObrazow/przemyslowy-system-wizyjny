518323aa39a4f5f0844aceeff527270ef3fefc1c
Stanisław Horna <stanislawhorna@outlook.com>
2024-04-29 19:45:59 +0200

    detect rings

--- main.py
+++ main.py
@@ -66,25 +66,40 @@ def mainThresholds():
     bd_earings = BlobDetector(
         filter_by_area=True,
         min_area=1000,
-        max_area=10800,
+        max_area=10900,
         filter_by_circularity=True,
-        min_circularity=0.3,
-        max_circularity=0.5,
+        min_circularity=0.45,
+        max_circularity=0.8,
         filter_by_convexity=True,
-        min_convexity=0.86,
+        min_convexity=0.20,
         max_convexity=0.9,
         filter_by_inertia=True,
-        min_inertia_ratio=0.1,
+        min_inertia_ratio=0.02,
         max_inertia_ratio=0.9
+    )
+    bd_rings = BlobDetector(
+        filter_by_area=True,
+        min_area=7000,
+        max_area=15000,
+        filter_by_circularity=True,
+        min_circularity=0.55,
+        max_circularity=1.0,
+        filter_by_convexity=True,
+        min_convexity=0.80,
+        max_convexity=1.0,
+        filter_by_inertia=True,
+        min_inertia_ratio=0.6,
+        max_inertia_ratio=1.0
     )
 
     while not video.is_ended():
         org_frame = video.get_frame()
         frame = cv2.cvtColor(org_frame, cv2.COLOR_BGR2GRAY)
-        thr = filters.threshold_minimum(frame)
-
-        thresh_for_frame = (frame >= thr)
-
+        thr = filters.threshold_triangle(frame)
+        # filters.try_all_threshold(frame)
+        # plt.show()
+        thresh_for_frame = (frame >= (thr - 22))
+        
         # fr = ndi.binary_fill_holes(thresh_for_frame)
         # brain_l, lb_num = measure.label(fr, return_num=True)
         # num_of_regions = np.max(brain_l)
@@ -97,18 +112,18 @@ def mainThresholds():
         frame_converted = np.uint8(thresh_for_frame) * 255
         filtered_frame = cv2.GaussianBlur(
             src=frame_converted,
-            ksize=(1, 1),
+            ksize=(7, 7),
             sigmaX=0,
             sigmaY=0
         )
 
         filtered_frame = cv2.Canny(
             image=filtered_frame,
-            threshold1=0,
-            threshold2=255
+            threshold1=50,
+            threshold2=250
         )
 
-        closing_mask = np.ones((5, 1), np.uint8)
+        closing_mask = np.ones((5, 2), np.uint8)
         closed_edges = cv2.morphologyEx(
             src=filtered_frame,
             op=cv2.MORPH_CLOSE,
@@ -119,21 +134,36 @@ def mainThresholds():
             mode=cv2.RETR_CCOMP,
             method=cv2.CHAIN_APPROX_SIMPLE
         )
-        keyPoints = bd_earings.detect_objects(Draw.contourFill(
-                frame,
-                contours,
-                Draw.COLOR_GREEN
-            ))
+        
+        innerContours = contours
+
+        # innerContours = []
+        # for i, contour in enumerate(contours):
+        #     if hierarchy[0][i][3] == -1:
+        #         innerContours.append(contour)
+
+        contoursFilled = Draw.contourFill(
+            frame,
+            innerContours,
+            Draw.COLOR_GREEN
+        )
+        kP_earings = bd_earings.detect_objects(contoursFilled)
+        kP_rings = bd_rings.detect_objects(contoursFilled)
 
         # video.show_frame(
-        #     filtered_frame
+        #     contoursFilled
         # )
-        video.show_frame(
+        frame_to_display = Draw.keyPoints(
             Draw.keyPoints(
                 frame,
-                keyPoints,
+                kP_earings,
                 Draw.COLOR_GREEN
-            )
+            ),
+            kP_rings,
+            Draw.COLOR_RED
+        )
+        video.show_frame(
+            frame_to_display
         )
 
 
