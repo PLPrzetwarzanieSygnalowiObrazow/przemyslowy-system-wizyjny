e12068c644dc591363396ffd7baa79e40ab0a15d
Stanislaw Horna <stanislawhorna@outlook.com>
2024-05-20 13:14:49 +0200

    Fix/run on lower resolution (#8)
    * update necklaces and earings
    
    * move blob detector params to separate file
    
    * remove printing size on video open
    
    * add rings detection

--- main.py
+++ main.py
@@ -10,64 +10,19 @@ import copy
 
 from dependencies.video import Video
 from dependencies.filter import Filter
-from dependencies.blobDetector import BlobDetector
+
 from dependencies.draw import Draw
 from dependencies.segmentation import Segmentation
+from dependencies.blobDetectorInit import *
 from dependencies.objectTracker import ObjectTracker
 
+
 VIDEO_FILE_PATH = "./assets/nagranie_v4_cut.mp4"
-EARINGS_DETECTOR = BlobDetector(
-    filter_by_color=True,
-    blob_color=0,
-    filter_by_area=True,
-    min_area=300,
-    max_area=2725,
-    filter_by_circularity=True,
-    min_circularity=0.35,
-    max_circularity=0.80,
-    filter_by_convexity=False,
-    min_convexity=0.20,
-    max_convexity=0.90,
-    filter_by_inertia=True,
-    min_inertia_ratio=0.12,
-    max_inertia_ratio=0.90,
-)
-RINGS_DETECTOR = BlobDetector(
-    filter_by_color=True,
-    blob_color=0,
-    filter_by_area=True,
-    min_area=1750,
-    max_area=3000,
-    filter_by_circularity=True,
-    min_circularity=0.6,
-    max_circularity=1,
-    filter_by_convexity=True,
-    min_convexity=0.6,
-    max_convexity=1,
-    filter_by_inertia=True,
-    min_inertia_ratio=0.5,
-    max_inertia_ratio=1,
-)
-NECKLES_DETECTOR = BlobDetector(
-    filter_by_color=True,
-    blob_color=0,
-    filter_by_area=True,
-    min_area=25000,
-    max_area=2500000,
-    filter_by_circularity=True,
-    min_circularity=0.25,
-    max_circularity=0.9,
-    filter_by_convexity=False,
-    min_convexity=0.1,
-    max_convexity=1.0,
-    filter_by_inertia=False,
-    min_inertia_ratio=0.4,
-    max_inertia_ratio=1,
-)
 
 
 def main():
-    video = Video(path=VIDEO_FILE_PATH, frame_no=0)
+    video = Video(path=VIDEO_FILE_PATH, frame_no=700)
+
     tracker = ObjectTracker()
 
     while (org_frame := video.get_frame()) is not None:
@@ -90,8 +45,8 @@ def main():
         contours_filled = Draw.contourFill(gray_frame, contours)
 
         # Zamknięcie krawędzi (tylko do pierścionków)
-        closed_frame = Filter.closing(canny_frame, 4)
-        # print(canny_frame.shape)
+        # closed_frame = Filter.closing(canny_frame, 4)
+
         # Znalezienie pierścionków
         rings_key_points = RINGS_DETECTOR.detect_objects(closed_frame)
 
@@ -99,14 +54,20 @@ def main():
         earings_key_points = EARINGS_DETECTOR.detect_objects(contours_filled)
 
         # Znalezienie naszyjników
-        neckles_key_points = NECKLES_DETECTOR.detect_objects(contours_filled)
+        necklaces_key_points = NECKLACES_DETECTOR.detect_objects(
+            contours_filled)
 
-        tracker.trackObjects(rings_key_points=rings_key_points)
+        # tracker.trackObjects(rings_key_points=rings_key_points)
         # Połączenie obrazu głównego z punktami
         # Wyszukiwanie pierścieni działa najlepiej
-        result = Draw.keyPoints(canny_frame, rings_key_points, Draw.COLOR_RED)
+        # if necklaces_key_points:
+        #     print("x: ", necklaces_key_points[0].pt[0])
+        #     # print("y: ", necklaces_key_points[0].pt[1])
+
+        result = Draw.keyPoints(
+            gray_frame, rings_key_points, Draw.COLOR_RED)
         result = Draw.keyPoints(result, earings_key_points, Draw.COLOR_GREEN)
-        result = Draw.keyPoints(result, neckles_key_points, Draw.COLOR_BLUE)
+        result = Draw.keyPoints(result, necklaces_key_points, Draw.COLOR_BLUE)
 
         # result = Draw.rectangle(
         #     result, [[(200, 0), (300, 300)]], color=Draw.COLOR_RED)
