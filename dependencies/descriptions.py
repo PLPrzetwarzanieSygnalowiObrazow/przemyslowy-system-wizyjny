c1ff771624ed633712b9a2d2608e1f610c917a66
Stanislaw Horna <stanislawhorna@outlook.com>
2024-05-21 18:27:03 +0200

    feature/cmdlineArgs (#11)
    * fix colors
    
    * NO TERAZ LICZY DOBRZE, ps argparse implemented

---
+++ dependencies/descriptions.py
@@ -0,0 +1,12 @@
+PROGRAM_DESCRIPTION = """
+An industrial vision system that will detect and then classify jewelry,
+placed on a moving conveyor belt in real time.
+"""
+
+VIDEO_FILE_PATH_HELPER = """
+Relative or absolute path to video file to analyze.
+"""
+
+START_FRAME_NUMBER_HELPER = """
+Frame number from which program starts analysis.
+"""
